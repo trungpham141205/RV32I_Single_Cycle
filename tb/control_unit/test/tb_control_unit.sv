a5f7cf59af02acd8f9e263176a29d1a33e80214c
trungpham141205 <pquoctrung141205@gmail.com>
2026-05-26 15:09:47 +0700

    new

--- tb/control_unit/test/tb_control_unit.sv
+++ tb/control_unit/test/tb_control_unit.sv
@@ -47,7 +47,7 @@ module tb_control_unit;
         input logic [6:0]i_opcode
     );
         
-        logic [16:0]i_expected;
+        control_t i_expected;
         funct7 = i_funct7;
         funct3 = i_funct3;
         opcode = i_opcode;
@@ -56,18 +56,55 @@ module tb_control_unit;
 
         if({reg_write, reg_back, imm_sel, src_a_sel, src_b_sel, alu_control, mem_read, mem_write, branch, jump, jump_reg} == i_expected) begin
             pass_count++;
-            $display("PASS | funct7 = %h | funct3 = %h | opcode = %h | result = %h", funct7, funct7, opcode, {reg_write, reg_back, imm_sel, src_a_sel, src_b_sel, alu_control, mem_read, mem_write, branch, jump, jump_reg});
+            $display("PASS | funct7 = %h | funct3 = %h | opcode = %h | result = %h", funct7, funct3, opcode, {reg_write, reg_back, imm_sel, src_a_sel, src_b_sel, alu_control, mem_read, mem_write, branch, jump, jump_reg});
         end
         else begin
             fail_count++;
-            $display("FAIl | funct7 = %h | funct3 = %h | opcode = %h | expected = %h | got = %h", funct7, funct7, opcode, i_expected, {reg_write, reg_back, imm_sel, src_a_sel, src_b_sel, alu_control, mem_read, mem_write, branch, jump, jump_reg});
+            $display("FAIl | funct7 = %h | funct3 = %h | opcode = %h | expected = %h | got = %h", funct7, funct3, opcode, i_expected, {reg_write, reg_back, imm_sel, src_a_sel, src_b_sel, alu_control, mem_read, mem_write, branch, jump, jump_reg});
         end
 
     endtask 
 
     //5. Initial Block
     initial begin
-        
-    end
+    // LUI
+    apply_and_check(1'b0, 3'b000, 7'b0110111);
+
+    // AUIPC
+    apply_and_check(1'b0, 3'b000, 7'b0010111);
+
+    // JAL
+    apply_and_check(1'b0, 3'b000, 7'b1101111);
+
+    // JALR
+    apply_and_check(1'b0, 3'b000, 7'b1100111);
+
+    // LOAD
+    apply_and_check(1'b0, 3'b010, 7'b0000011);
+
+    // S-TYPE
+    apply_and_check(1'b0, 3'b010, 7'b0100011);
+
+    // B-TYPE
+    apply_and_check(1'b0, 3'b000, 7'b1100011); // BEQ
+    apply_and_check(1'b0, 3'b001, 7'b1100011); // BNE
+    apply_and_check(1'b0, 3'b100, 7'b1100011); // BLT
+    apply_and_check(1'b0, 3'b110, 7'b1100011); // BLTU
+
+    // I-TYPE
+    apply_and_check(1'b0, 3'b000, 7'b0010011); // ADDI
+    apply_and_check(1'b0, 3'b001, 7'b0010011); // SLLI
+    apply_and_check(1'b0, 3'b101, 7'b0010011); // SRLI
+    apply_and_check(1'b1, 3'b101, 7'b0010011); // SRAI
+
+    // R-TYPE
+    apply_and_check(1'b0, 3'b000, 7'b0110011); // ADD
+    apply_and_check(1'b1, 3'b000, 7'b0110011); // SUB
+    apply_and_check(1'b0, 3'b101, 7'b0110011); // SRL
+    apply_and_check(1'b1, 3'b101, 7'b0110011); // SRA
+
+    $display("Result: %0d PASSED, %0d FAILED", pass_count, fail_count);
+    $finish;
+end
     
 endmodule
